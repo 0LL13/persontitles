8257f20fea24cd0a797e456702a82f75a53bdf19
Oliver Stapel <hardy.ecc95@gmail.com>
2021-07-12 14:35:47 +0200

    Now version 0.2.3

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/0LL13/persontitles',
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
     extras_require={
         'dev': ['check-manifest'],
